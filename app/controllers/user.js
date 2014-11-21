ee7a151ee6392037721b546b2a1ada794f8136e5
Jonathan <modjow.jm9@gmail.com>
2014-11-21 20:18:50 +0100

    Test Login

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -3,22 +3,20 @@ app.controller('UserCtrl',function($scope, $http){
     $scope.user = {};
 
     $scope.submit = function() {
-    	console.log($scope.user.username);
-    	console.log(hex_sha1("a"));
 
 	    $http({
 
 	        url: "http://mercandalli.com/Jarvis-API/",
 	        data: $scope.form,
 	        method: 'GET',
-	        headers : {'Authorization-Type':'Basic bashe64usename:password'}
+	        headers : {'Authorization-Type':'Basic '+$scope.user.username+':password'+hex_sha1($scope.user.password)}
 
 	    }).success(function(data){
 
 	        console.log("OK", data)
 
 	    }).error(function(err){"ERR", console.log(err)})
-		
+
 	}
 
 });
