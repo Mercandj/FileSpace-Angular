c9308a1d0fd32ff8f445eaec77eca36a14262962
Jonathan <modjow.jm9@gmail.com>
2014-11-22 14:01:22 +0100

    Test (10)

--- app/services/file.js
+++ app/services/file.js
@@ -1,43 +1,47 @@
-app.factory('FileFactory', function($scope, $location, $http, $q, myCache) {
+app.factory('FileFactory', ['$scope', '$location', '$http', '$q', 'myCache',
+    function($scope, $location, $http, $q, myCache) {
 
-    /*
-    var factory = {
-        file : false,
-        all : function() {
-            var deferred = $q.defer();
+        
+        var factory = {
+            file : false,
+            all : function() {
+                
+                var deferred = $q.defer();
+                /*
+                if(factory.file !== false) {
+                    console.log('file.js : factory.file !== false');
+                    deferred.resolve(factory.fil);
+                }
+                else {
+                    $http({
 
-            if(factory.file !== false) {
-                console.log('file.js : factory.file !== false');
-                deferred.resolve(factory.fil);
-            }
-            else {
-                $http({
+                        url: URL_SERVER+'file',
+                        method: 'GET',
+                        headers : {
+                            'Authorization':'Basic '+ myCache.get('myData'),
+                            'Content-Type':'application/json',
+                        }
 
-                    url: URL_SERVER+'file',
-                    method: 'GET',
-                    headers : {
-                        'Authorization':'Basic '+ myCache.get('myData'),
-                        'Content-Type':'application/json',
-                    }
+                    })
+                    .success(function(data,status) {
+                        factory.file = data;
+                        deferred.resolve(factory.file);
+                    })
+                    .error(function(data,status) {
+                        if(status == 401)
+                            deferred.reject('401 unauthorized')
+                        else if(status == 404)
+                            deferred.reject('404 not found');
+                        else
+                            deferred.reject('Cannot get files');
+                    });
+                }
+                */
+                return deferred.promise;
+            },
+        }
 
-                })
-                .success(function(data,status) {
-                    factory.file = data;
-                    deferred.resolve(factory.file);
-                })
-                .error(function(data,status) {
-                    if(status == 401)
-                        deferred.reject('401 unauthorized')
-                    else if(status == 404)
-                        deferred.reject('404 not found');
-                    else
-                        deferred.reject('Cannot get files');
-                });
-            }
-            return deferred.promise;
-        },
+        return factory;
+        
     }
-
-    return factory;
-    */
-})
+]);
