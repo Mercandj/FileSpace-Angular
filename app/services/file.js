7f053503b1f0ce59c6bbdf30e84f4912f8842624
Jonathan <modjow.jm9@gmail.com>
2014-11-22 11:41:32 +0100

    Test

--- app/services/file.js
+++ app/services/file.js
@@ -1,4 +1,4 @@
-app.factory('FileFactory', function($http, $q, myCache){
+app.factory('FileFactory', function($scope, $location, $http, $q, myCache){
     var factory = {
         files : false,
         all : function() {
