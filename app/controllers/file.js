b30358153b5c03dc7df438532e678fdeade93b81
Jonathan <modjow.jm9@gmail.com>
2014-11-22 14:10:08 +0100

    Test (14)

--- app/controllers/file.js
+++ app/controllers/file.js
@@ -1,5 +1,5 @@
 app.controller('FileCtrl',
-	function($scope, FileFactory) {
+	function($scope, $location, $http, $q, Base64, myCache) {
 	/*
     $scope.file = FileFactory.all().then(function(file) {
         $scope.file = file;
@@ -7,5 +7,30 @@ app.controller('FileCtrl',
         alert(msg);
     });
 	*/
+
+		$http({
+
+            url: URL_SERVER+'file',
+            method: 'GET',
+            headers : {
+                'Authorization':'Basic '+ myCache.get('myData'),
+                'Content-Type':'application/json',
+            }
+
+        })
+        .success(function(data,status) {
+            console.log(status + " : " + JSON.stringify(data));
+        })
+        .error(function(data,status) {
+            if(status == 401)
+                deferred.reject('401 unauthorized')
+            else if(status == 404)
+                deferred.reject('404 not found');
+            else
+                deferred.reject('Cannot get files');
+        });
+
+
+
 	}
 );
