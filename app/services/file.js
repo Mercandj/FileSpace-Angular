14cbbb93a0113f843ea93fc2b9980e8ec8fab262
Jonathan <modjow.jm9@gmail.com>
2014-11-22 11:55:03 +0100

    Test (4)

--- app/services/file.js
+++ app/services/file.js
@@ -1,31 +1,32 @@
-app.factory('FileFactory', function($scope, $location, $http, $q, myCache){
+app.factory('FileFactory', 
+    function($scope, $location, $http, $q, myCache){
 
-    $http({
+        $http({
 
-        url: URL_SERVER+'file',
-        method: 'GET',
-        headers : {
-            'Authorization':'Basic '+ myCache.get('myData'),
-            'Content-Type':'application/json',
-        }
+            url: URL_SERVER+'file',
+            method: 'GET',
+            headers : {
+                'Authorization':'Basic '+ myCache.get('myData'),
+                'Content-Type':'application/json',
+            }
 
-    })
-    .success(function(data, status, headers, config) {
-        console.log(status + " : " + JSON.stringify(data));
+        })
+        .success(function(data, status, headers, config) {
+            console.log(status + " : " + JSON.stringify(data));
 
-        if(data.succeed === true) {
-            
-        }
+            if(data.succeed === true) {
+                
+            }
 
-    })
-    .error(function(data, status, headers, config) {
-        if(status == 401)
-            deferred.reject('401 unauthorized');
-        else if(status == 404)
-            deferred.reject('404 not found');
-        else
-            deferred.reject('Cannot get user');
-    });
+        })
+        .error(function(data, status, headers, config) {
+            if(status == 401)
+                deferred.reject('401 unauthorized');
+            else if(status == 404)
+                deferred.reject('404 not found');
+            else
+                deferred.reject('Cannot get user');
+        });
 
-
-})
+    }
+);
