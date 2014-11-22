f679cbfdf4d9b4bf2f87a1c0adfcd8048359949c
Jonathan <modjow.jm9@gmail.com>
2014-11-22 22:37:36 +0100

    Test

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -4,7 +4,16 @@ app.controller('UserCtrl',
 	    $scope.user = {};
 	    var deferred = $q.defer();
 
-	    $scope.submit = function(){userService.login();}/*function() {
+	    $scope.submit = function() {
+	    	userService.login(
+	    		URL_SERVER+'user',
+	    		Base64.encode($scope.user.username + ':' + hex_sha1($scope.user.password)),
+	    		$scope.form
+	    	);
+	    }
+
+
+	    /*function() {
 
 		    $http({
 
