75c53431550632dbc4edb7e7e0708de6d824a061
Mercandj <modjow.jm9@gmail.com>
2015-03-04 23:55:20 +0100

    Fix files download

--- controllers/file.js
+++ controllers/file.js
@@ -76,24 +76,126 @@ app.controller('FileCtrl',
             $http({
                 url: URL_SERVER+'file/'+file.id,
                 method: 'GET',
+                responseType:'arraybuffer',
                 headers : {
-                    'Authorization':'Basic '+ myCache.get('myData'),
-                    'Content-Type':'application/json',
+                    'Authorization':'Basic '+ myCache.get('myData')
                 }
             })
-            .success(function(data,status) {
-                var fileTMP = new Blob([ data ], {
-                    type : 'application/csv'
-                });
-                //trick to download store a file having its URL
-                var fileURL   = URL.createObjectURL(fileTMP);
-                var a         = document.createElement('a');
-                a.href        = fileURL; 
-                a.target      = '_blank';
-                a.download    = file.name + '.' + file.type;
-                document.body.appendChild(a);
-                a.click();
+            .success(function(data,status,headers) {
+                
+                var octetStreamMime = "application/octet-stream";
+                switch(file.type) {
+                    case 'pdf':     octetStreamMime = 'application/pdf';      break;
+                    case 'zip':     octetStreamMime = 'application/zip';      break;
+                    case 'jpeg':    octetStreamMime = 'image/jpg';            break;
+                    case 'jpg':     octetStreamMime = 'image/jpg';            break;
+                    case 'png':     octetStreamMime = 'image/png';            break;
+                    case 'gif':     octetStreamMime = 'image/gif';            break;
+                    case 'html':    octetStreamMime = 'image/html';           break;
+                    case 'doc':     octetStreamMime = 'image/msword';         break;
+                    case 'mp3':     octetStreamMime = 'audio/mpeg';           break;
+                    case 'apk':     octetStreamMime = 'application/vnd.android.package-archive'; break;
+                }
+                
+                var success = false;
+
+                // Get the headers
+                headers = headers();
+
+                // Get the filename from the x-filename header or default to "download.bin"
+                var filename = headers['x-filename'] || file.name;
+
+                // Determine the content type from the header or default to "application/octet-stream"
+                var contentType = headers['content-type'] || octetStreamMime;
+
+                try
+                {
+                    // Try using msSaveBlob if supported
+                    console.log("Trying saveBlob method ...");
+                    var blob = new Blob([data], { type: contentType });
+                    if(navigator.msSaveBlob)
+                        navigator.msSaveBlob(blob, filename);
+                    else {
+                        // Try using other saveBlob implementations, if available
+                        var saveBlob = navigator.webkitSaveBlob || navigator.mozSaveBlob || navigator.saveBlob;
+                        if(saveBlob === undefined) throw "Not supported";
+                        saveBlob(blob, filename);
+                    }
+                    console.log("saveBlob succeeded");
+                    success = true;
+                } catch(ex)
+                {
+                    console.log("saveBlob method failed with the following exception:");
+                    console.log(ex);
+                }
+
+                if(!success)
+                {
+                    // Get the blob url creator
+                    var urlCreator = window.URL || window.webkitURL || window.mozURL || window.msURL;
+                    if(urlCreator)
+                    {
+                        // Try to use a download link
+                        var link = document.createElement('a');
+                        if('download' in link)
+                        {
+                            // Try to simulate a click
+                            try
+                            {
+                                // Prepare a blob URL
+                                console.log("Trying download link method with simulated click ...");
+                                var blob = new Blob([data], { type: contentType });
+                                var url = urlCreator.createObjectURL(blob);
+                                link.setAttribute('href', url);
+
+                                // Set the download attribute (Supported in Chrome 14+ / Firefox 20+)
+                                link.setAttribute("download", filename);
+
+                                // Simulate clicking the download link
+                                var event = document.createEvent('MouseEvents');
+                                event.initMouseEvent('click', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
+                                link.dispatchEvent(event);
+                                console.log("Download link method with simulated click succeeded");
+                                success = true;
+
+                            } catch(ex) {
+                                console.log("Download link method with simulated click failed with the following exception:");
+                                console.log(ex);
+                            }
+                        }
+
+                        if(!success)
+                        {
+                            // Fallback to window.location method
+                            try
+                            {
+                                // Prepare a blob URL
+                                // Use application/octet-stream when using window.location to force download
+                                console.log("Trying download link method with window.location ...");
+                                var blob = new Blob([data], { type: octetStreamMime });
+                                var url = urlCreator.createObjectURL(blob);
+                                window.location = url;
+                                console.log("Download link method with window.location succeeded");
+                                success = true;
+                            } catch(ex) {
+                                console.log("Download link method with window.location failed with the following exception:");
+                                console.log(ex);
+                            }
+                        }
+
+                    }
+                }
+
+                if(!success)
+                {
+                    // Fallback to window.open method
+                    console.log("No methods worked for saving the arraybuffer, using last resort window.open");
+                    window.open(httpPath, '_blank', '');
+                }
+                
+
             });
+
         };
 
         $scope.edit = function(file) {
@@ -108,7 +210,7 @@ app.controller('FileCtrl',
                     }
                 })
                 .success(function(data,status) {
-                    openDialog(file.url, "", 
+                    openDialog(file.name, "", 
                         '<textarea rows="5" name="text" placeholder="YOUR TXT" class="error">'+
                         data +
                         '</textarea>'+
