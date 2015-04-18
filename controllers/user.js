35d9dc6722678874906d16dda74c16d78b36b1b5
Jonathan <modjow.jm9@gmail.com>
2015-04-18 14:00:11 +0200

    Update new password (sha1(pass+date) see API code isUser())

--- controllers/user.js
+++ controllers/user.js
@@ -5,9 +5,16 @@ app.controller('UserCtrl',
 	    var deferred = $q.defer();
 
 	    $scope.submit = function() {
+	    	var now = new Date();
+	    	var year = now.getUTCFullYear();
+	    	var month = now.getUTCMonth()<10 ? ('0' + now.getUTCMonth()) : now.getUTCMonth();
+	    	var day = now.getUTCDate()<10 ? ('0' + now.getUTCDate()) : now.getUTCDate();
+	    	var hours = now.getUTCHours()<10 ? ('0' + now.getUTCHours()) : now.getUTCHours();
+	    	var minutes = now.getUTCMinutes()<10 ? ('0' + now.getUTCMinutes()) : now.getUTCMinutes();
+	    	var date_str = year+"-"+month+"-"+day+" "+hours+":"+minutes;
 	    	userService.login(
 	    		URL_SERVER+'user',
-	    		Base64.encode($scope.user.username + ':' + hex_sha1($scope.user.password)),
+	    		Base64.encode($scope.user.username + ':' + hex_sha1( hex_sha1(hex_sha1($scope.user.password)) + date_str )),
 	    		$scope.form
 	    	);
 	    }
