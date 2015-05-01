928658d7ed3ef8049ab94004b1db2dd4acd9e555
Mercandj <modjow.jm9@gmail.com>
2015-05-01 16:19:16 +0100

    Update file.js

--- controllers/file.js
+++ controllers/file.js
@@ -29,9 +29,49 @@ app.controller('FileCtrl',
                         file.icon='directory.png';
                     else
                         file.icon='file_default.png';
-                });                
+                });
                 $scope.filesOnline = data.result;
             	deferred.resolve(data.result);
+            	
+            	var DateDiff = {
+                        inSeconds: function(d1, d2) {
+                        var t2 = d2.getTime(), t1 = d1.getTime();
+                        return parseInt((t2-t1)/(1000));
+                    },
+                    inMinutes: function(d1, d2) {
+                        var t2 = d2.getTime(), t1 = d1.getTime();
+		        return parseInt((t2-t1)/(60*1000));
+                    },
+                    inHours: function(d1, d2) {
+		        var t2 = d2.getTime(), t1 = d1.getTime();
+		        return parseInt((t2-t1)/(3600*1000));
+                    },
+                    inDays: function(d1, d2) {
+		        var t2 = d2.getTime(), t1 = d1.getTime();
+		        return parseInt((t2-t1)/(24*3600*1000));
+                    },
+                    inMonths: function(d1, d2) {
+		        var d1Y = d1.getFullYear(), d2Y = d2.getFullYear(), d1M = d1.getMonth(), d2M = d2.getMonth();
+		        return (d2M+12*d2Y)-(d1M+12*d1Y);
+                    },
+		    inYears: function(d1, d2) {
+		        return d2.getFullYear()-d1.getFullYear();
+                    }
+		};
+		
+		data.result.forEach(function(file) {
+                    if(file.type=="jarvis") {
+                        var element_timer = document.querySelector('.file-type-'+file.id+' .file-type-jarvis'),
+                            jsonDate = JSON.parse(file.content),
+                            timerDate = new Date(jsonDate.timer_date.replace(" ", "T") + "Z"),
+                    	    interval = setInterval(function(timerDate) {
+		            element_timer.innerHTML = 
+		        		(DateDiff.inHours(new Date(), timerDate)%24) + " " +
+		        		(DateDiff.inMinutes(new Date(), timerDate)%60)  + " " +
+		        		(DateDiff.inSeconds(new Date(), timerDate)%60)  ;
+		        }, 1000, timerDate);
+                    }
+                });
             }
         })
         .error(function(data,status) {
