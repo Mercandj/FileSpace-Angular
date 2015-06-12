2f36bc5c5cc8f8bd1087a64e1ed095ea2ab8372e
Jonathan <modjow.jm9@gmail.com>
2015-06-13 01:39:53 +0200

    Test

--- services/file.js
+++ services/file.js
@@ -45,7 +45,7 @@ app.factory('FileFactory',
 );
 
 app.service('fileService', ['$http', 'myCache', '$q',
-    function ($http, myCache, $q) {
+    function ($scope, $http, myCache, $q) {
         this.uploadFileToUrl = function(p_url, p_auth, p_filesArray) {
 
             var deferred = $q.defer();
