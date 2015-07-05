a0d48a866696bcd232b866fedf43b95d195b17ac
Jonathan <modjow.jm9@gmail.com>
2015-07-05 19:21:17 +0200

    Test

--- services/user.js
+++ services/user.js
@@ -6,7 +6,6 @@ app.service('userService', ['$location', '$http', 'myCache',
             $http({
 
                 url: p_url,
-                data: p_data,
                 method: 'POST',
                 transformRequest: function(p_data) {
                     var str = [];
